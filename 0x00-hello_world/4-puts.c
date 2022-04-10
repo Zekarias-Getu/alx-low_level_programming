e42ade9a9dc63ae4a02765712bddc00e06de7bd4
Zekarias-Getu <zekeyg070@gmail.com>
2022-04-10 23:22:55 +0300

    add 4-put.c script

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point to c lang
+ *
+ * Return: returns always 0
+ */
+int main(void)
+{
+	/* displays the char inside quotation */
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
