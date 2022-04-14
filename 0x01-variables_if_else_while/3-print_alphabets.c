b897771454dc0117fcdd0b63830eca0288faec28
Zekarias-Getu <zekeyg070@gmail.com>
2022-04-15 01:09:17 +0300

    add 3-print_alpahabets.c script

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+*main-entry to c program 
+*
+*desc:this program will print all letter of english lang both upper and lower case 
+*
+*retuern:returns zero
+*/
+int main(void)
+{
+char letter;
+
+for (letter = 'a'; letter <= 'z'; letter++)
+putchar(letter);
+
+for (letter='A'; letter <='z'; letter++)
+putchar(letter);
+
+putchar('\n');
+
+return(0);
+}
