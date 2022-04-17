9c4b04a82f479ee55962c85bedc59ce651041587
Zekarias-Getu <zekeyg070@gmail.com>
2022-04-17 15:02:23 +0300

    add script

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * print_alphabet - check the code
- *
- * Return: Always 0.
- */
+* print_alphabet - check the code
+*
+* Return: Always 0.
+*/
 void print_alphabet(void)
 {
-	int i;
+int i;
 
-	for (i = 'a'; i <= 'z'; i++)
-		_putchar(i);
-	_putchar('\n');
+for (i = 'a'; i <= 'z'; i++)
+_putchar(i);
+_putchar('\n');
 }
