43ee522942f26bac2d6b2c0db4a74fabe0e5ebf5
Zekarias-Getu <zekeyg070@gmail.com>
2022-04-18 18:18:32 +0300

    add 2-print_alpahabet_x.c script

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*print_alphabet_x10 - check the code.
+*
+*Return: Always 0.
+*/
+void print_alphabet_x10(void)
+{
+int j = 0;
+int i;
+while (j < 10)
+{
+for (i = 'a'; i <= 'z'; i++)
+_putchar(i);
+_putchar('\n');
+j++;
+}
+}
