a8f5671edc42575aae33ee6f981a4d0b26fae3dd
Zekarias-Getu <zekeyg070@gmail.com>
2022-04-20 22:46:18 +0300

    add check for digit

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,42 +1,30 @@
 #include "main.h"
 
 /**
-* is_numerical - check if digit or not
-* @n: number
-* Return: 1 if it is a number 0 otherwise
+* _atoi - convert a string to an integer.
+*@s: pointer
+* Return: Always 0 (Success)
 */
-int is_numerical(unsigned int n)
-{
-	return (n >= '0' && n <= '9');
-}
-
-/**
-* _atoi - convert a string to an integer
-* @s: the string to be converted
-*
-* Return: 0 if no numbers in the string, otherwise numbers times sign
-*/
-
 int _atoi(char *s)
 {
-	unsigned int num, i;
-	int sign = 1;
-
-	num = 0;
+	int nev = 1;
+	int i;
+	unsigned int num = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (is_numerical(s[i]))
+		if (s[i] == '-')
 		{
-			num = (s[i] - 48) + num * 10;
-			if (s[i + 1] == ' ')
-				break;
+			nev *= -1;
 		}
-		else if (s[i + 1] == '-')
+		else if (s[i] >= '0' && s[i] <= '9')
 		{
-			sign *= -1;
+			num = (num * 10) + (s[i] - '0');
+		}
+		else if (num > 0)
+		{
+			break;
 		}
 	}
-
-		return (num * sign);
+	return (num * nev);
 }
