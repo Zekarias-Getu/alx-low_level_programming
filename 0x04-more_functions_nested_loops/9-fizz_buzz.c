23370ece7a6753661d733025cd4780387e66ff08
Zekarias-Getu <zekeyg070@gmail.com>
2022-04-20 20:29:11 +0300

    add print fizz buzz script

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+* main - prints number from zero to hundres
+*
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 5 == 0)
+		{
+			if (i < 100)
+				printf("Buzz ");
+			else
+				printf("Buzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+	return (0);
+}
