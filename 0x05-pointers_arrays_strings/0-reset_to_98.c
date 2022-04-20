201a859ca3441220b5667541e254a21d49cbb43d
Zekarias-Getu <zekeyg070@gmail.com>
2022-04-20 21:11:10 +0300

    add pointer as parameter script

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* reset_to_98 - takes a pointer as parameter and change the value
+* @n: pointer name
+*
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+# 0x05. C - Pointers, arrays and strings
+* Pointers and arrays
+* Data Structures 
