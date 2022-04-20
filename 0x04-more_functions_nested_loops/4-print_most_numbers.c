06b6ee72a4d449d96cd33ab4b024cf4045a58d5e
Zekarias-Getu <zekeyg070@gmail.com>
2022-04-20 20:17:16 +0300

    add print most numbers script

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_most_numbers - print the number except 2 and 4
+*
+*/
+
+void print_most_numbers(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		if (x == 2 || x == 4)
+			x++;
+
+		_putchar(x + '0');
+	}
+	_putchar('\n');
+}
