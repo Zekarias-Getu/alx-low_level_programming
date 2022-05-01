041ba4a6de4310d2078a03f3e1cc67c6f1f51f98
zekarias_getu <zekeyg070@gmail.com>
2022-05-01 22:24:20 +0300

    Even more pointers

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * _memcpy - copies memory area
- * @dest:char
- * @src:char
- * @n:int
- * Return:char pointer
- */
+* _memcpy - copies memory area
+* @dest:char
+* @src:char
+* @n:int
+* Return:char pointer
+*/
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 		unsigned int i;
